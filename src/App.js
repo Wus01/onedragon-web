1d4cc51531d14d5ada3b800c79954b7919bfc611
wj5027 <45251381+wj5027@users.noreply.github.com>
2025-10-05 16:40:43 +0900

    템플릿 login, register 페이지만 추가

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
 import './App.css';
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import Login from './app/user-pages/Login';
+import Register from './app/user-pages/Register';
 
 function App() {
   return (
+
     <div className="App">
-        <p>일용이네</p>
+
+        <Routes>
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+            {/* 기존 라우트들 */}
+        </Routes>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import { BrowserRouter } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';  // 부트스트랩 스타일 추가
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <React.StrictMode>
+        <BrowserRouter>
+            <App />
+        </BrowserRouter>
+    </React.StrictMode>
 );
