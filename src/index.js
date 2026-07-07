46118d26faefbc8947061d349fb7b7d678c4f09f
wj5027 <45251381+wj5027@users.noreply.github.com>
2026-07-07 17:08:28 +0900

    Fix: basename 빈 값으로 수정 및 홈페이지 경로 제거

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import "./i18n";
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
-  <BrowserRouter basename="/">
+  <BrowserRouter basename="">
     <App />
   </BrowserRouter>
 , document.getElementById('root'));
